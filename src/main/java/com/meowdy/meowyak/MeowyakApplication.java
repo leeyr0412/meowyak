c54c4930b9c534d8e9fdfa035f43e2aac43e7ae1
yeri <leeyr0412@naver.com>
2026-05-05 00:35:46 +0900

    chore: 프로젝트 초기 설정 (Spring Boot 3.5.0, Java 17)

---
+++ src/main/java/com/meowdy/meowyak/MeowyakApplication.java
@@ -0,0 +1,13 @@
+package com.meowdy.meowyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowyakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meowdy/meowyak/MeowyakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meowdy.meowyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowyakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
